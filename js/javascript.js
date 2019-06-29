36a633ea826f50edceb505ae4d465e08855923c7
Agnieszka Kot <agnieszkakot22@gmail.com>
2019-06-30 00:07:34 +0200

    Task-8.2-b

--- js/javascript.js
+++ js/javascript.js
@@ -3,24 +3,27 @@ var output = document.getElementById('greeter-output');
 
 var button = document.getElementById('greeter-button');
 var temp;
+var tempf
 
 button.addEventListener('click', function () {
     temp = window.prompt('What is the temperature today?')
-    if (temp == null || temp == undefined || temp =='') {
-        output.innerHTML = 'Invalid number' + '<br><br>' + output.innerHTML;
-    }
-     else {
+    tempf = (temp * 1.8) + 32
+    if ((temp == null || temp == undefined || temp == '')) {
+        output.innerHTML = "Value can't be empty" + '<br><br>' + output.innerHTML;
+    } else if (isNaN(temp)) {
+        output.innerHTML = "Not a number" + '<br><br>' + output.innerHTML;
+    } else {
         if (temp < 0) {
-            output.innerHTML = 'Temperature in Celsius is: ' + temp + '<br>' + 'Temperature in Farenheit is: ' + ((temp * 1.8) + 32) + '<br>' + 'Ubierz ciepłą kurtkę i szalik, bo woda zamarza!' + '<br><br>' + output.innerHTML;
+            output.innerHTML = 'Temperature in Celsius is: ' + temp + '<br>' + 'Temperature in Farenheit is: ' + tempf + '<br>' + 'Ubierz ciepłą kurtkę i szalik, bo woda zamarza!' + '<br><br>' + output.innerHTML;
         } else if (temp >= 0 && temp < 75) {
             if (temp < 15) {
-                output.innerHTML = 'Temperature in Celsius is: ' + temp + '<br>' + 'Temperature in Farenheit is: ' + ((temp * 1.8) + 32) + '<br><br>' + 'Ubierz lekką kurtkę' + '<br><br>' + 'Woda jest jeszcze w stanie ciekłym' + '<br><br>' + output.innerHTML;
+                output.innerHTML = 'Temperature in Celsius is: ' + temp + '<br>' + 'Temperature in Farenheit is: ' + tempf + '<br><br>' + 'Ubierz lekką kurtkę' + '<br><br>' + 'Woda jest jeszcze w stanie ciekłym' + '<br><br>' + output.innerHTML;
             } else {
-                output.innerHTML = 'Temperature in Celsius is: ' + temp + '<br>' + 'Temperature in Farenheit is: ' + ((temp * 1.8) + 32) + '<br><br>' + 'Krutkie spodenki będą spoko!' + '<br><br>' + 'Woda jest jeszcze w stanie ciekłym' + '<br><br>' + output.innerHTML;
+                output.innerHTML = 'Temperature in Celsius is: ' + temp + '<br>' + 'Temperature in Farenheit is: ' + tempf + '<br><br>' + 'Krutkie spodenki będą spoko!' + '<br><br>' + 'Woda jest jeszcze w stanie ciekłym' + '<br><br>' + output.innerHTML;
             }
 
         } else {
-            output.innerHTML = 'Temperature in Celsius is: ' + temp + '<br>' + 'Temperature in Farenheit is: ' + ((temp * 1.8) + 32) + '<br><br>' + 'Woda już paruje!' + '<br><br>' + output.innerHTML;
+            output.innerHTML = 'Temperature in Celsius is: ' + temp + '<br>' + 'Temperature in Farenheit is: ' + tempf + '<br><br>' + 'Woda już paruje!' + '<br><br>' + output.innerHTML;
         }
 
     }
@@ -31,17 +34,17 @@ button.addEventListener('click', function () {
 
 var output2 = document.getElementById('greeter-output2');
 
-var button = document.getElementById('greeter-button2');
-var temp;
+var button2 = document.getElementById('greeter-button2');
 var tempc;
 
-button.addEventListener('click', function () {
+button2.addEventListener('click', function () {
     temp = window.prompt('What is the temperature today?')
     tempc = (temp - 32) / 1.8
-    if (temp == null || temp == undefined || temp =='') {
-        output2.innerHTML = 'Invalid number' + '<br><br>' + output.innerHTML;
-    }
-    else {
+    if ((temp == null || temp == undefined || temp == '')) {
+        output2.innerHTML = "Value can't be empty" + '<br><br>' + output2.innerHTML;
+    } else if (isNaN(temp)) {
+        output2.innerHTML = "Not a number" + '<br><br>' + output2.innerHTML;
+    } else {
         if (tempc < 0) {
             output2.innerHTML = 'Temperature in Celsius is: ' + tempc + '<br>' + 'Temperature in Farenheit is: ' + temp + '<br>' + 'Ubierz ciepłą kurtkę i szalik, bo woda zamarza!' + '<br><br>' + output2.innerHTML;
         } else if (tempc >= 0 && tempc < 75) {
